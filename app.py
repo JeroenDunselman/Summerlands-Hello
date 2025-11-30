2666d82264555a6d23f928ff619e1495b6ec43ff
Jeroen Dunselman <jeroduns@gmail.com>
2025-11-30 21:09:22 +0100

    Add Hello World app

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Summerlands – Hello World", layout="centered")
+st.title("Summerlands – Hello World")
+st.balloons()
+
+st.success("Het werkt! Summerlands leeft weer!")
+st.write("Nu kun je de echte tartan-app hier weer in zetten.")
+st.caption("Gemaakt door jou – met trots")
